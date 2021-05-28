5ff7fbc905defc4a8c43c9a666df1ad3fccf44ec
chiguayeshao <chiguayeshao@gmail.com>
2021-05-28 15:56:40 +0800

    add http post

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-add-task',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTaskComponent implements OnInit {
 
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter()
+
   text: string;
   day: string;
   reminder: boolean = false;
@@ -14,6 +17,29 @@ export class AddTaskComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task...')
+      return
+    }
+    if (!this.day) {
+      alert('Please add a day...')
+      return
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
   }
 
 }
